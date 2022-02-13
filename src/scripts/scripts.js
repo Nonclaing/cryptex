13482c6eb3e5aeb315e5997faf6160abd6f22e1d
Nonclaing <32881294+Nonclaing@users.noreply.github.com>
2022-02-13 15:59:24 +0300

    готово

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -19,5 +19,29 @@ $(document).ready(function() {
             }
         ],
     });
-    console.log(1)
+
+    $('.accordion__button').click(function (event) {
+        // класс one закрывает другие вкладки
+        // класс active будет висеть не на title, а на блоке item
+
+        // Сохраним переменную item и блока, чтобы проверять их в дальнейшем
+        // item - блок с заголовком, на который нажали
+        // block - блок со списком всех заголовков
+        let item = $(this).parent();
+        let block = item.parent();
+        console.log(block);
+        // Проверка на наличие класса one у блока
+        if (block.hasClass('one')) {
+            // Если класс есть, то требуется закрыть все остальные заголовки
+
+            // Проверяем все item-ы и удаляем у всех, кроме текущего, класс active
+            block.children().not(item).removeClass('active');
+            // проверяем все block__text у всех item-ов в block, и если block__text соответствует текущему($(this).next()), то слайдим его вниз
+            block.children().children('.accordion__text').not($(this).next()).slideUp(300);
+        }
+        // добавялем класс active нашему item-у, на title которого мы нажали
+        $(this).toggleClass('active');
+        // слайдим текст в нашем блоке
+        block.children('.accordion__text').slideToggle(300)
+    });
 });
